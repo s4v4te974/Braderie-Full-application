5e5bb96f5808f9c367e429cff7d329fe57c81dbb
sandrinecab <cabrolsandrine@outlook.com>
2021-09-01 16:12:35 +0200

    method get UserRestcontroller

--- SpringAngularBraderie/src/main/java/com/springangularbraderie/restcontroller/UserRestController.java
+++ SpringAngularBraderie/src/main/java/com/springangularbraderie/restcontroller/UserRestController.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -51,14 +52,25 @@ public class UserRestController {
 //		return p_user;
 //	}
 
-	@PostMapping(path="/user", produces= "application/json")
-	public User getCurrentConnectUser(@RequestBody Map<String, String> json) {
+//	@PostMapping(path="/user", produces= "application/json")
+//	public User getCurrentConnectUser(@RequestBody Map<String, String> json) {
+//		
+//		String login = json.get("login");
+//		
+//		String pass = json.get("pass");
+//		
+//		User p_user = hUserService.enableTolog(login, pass);
+//	
+//		log.info("user authentifié : " + p_user);
+//		
+//		return p_user;
+//	}
+	
+	
+	@GetMapping(path="/user", produces= "application/json")
+	public User getCurrentConnectUser(@RequestBody User p_user) {
 		
-		String login = json.get("login");
-		
-		String pass = json.get("pass");
-		
-		User p_user = hUserService.enableTolog(login, pass);
+		p_user = hUserService.enableTolog(p_user.getLogin(), p_user.getPass());
 	
 		log.info("user authentifié : " + p_user);
 		
