fd8955429b4042440e5e10df151a6b444a2899a6
kingd <kingd@192.168.0.17>
2021-09-10 08:37:44 +0200

    Maj AdminController

--- SpringAngularBraderie/src/main/java/com/springangularbraderie/restcontroller/AdminRestController.java
+++ SpringAngularBraderie/src/main/java/com/springangularbraderie/restcontroller/AdminRestController.java
@@ -6,6 +6,7 @@ package com.springangularbraderie.restcontroller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springangularbraderie.model.Article;
 import com.springangularbraderie.service.AdminService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -14,34 +15,58 @@ import lombok.extern.slf4j.Slf4j;
  * @author Stephane Kouotze CDA7
  *
  */
+/**
+ * @author kingd
+ *
+ */
+/**
+ * @author kingd
+ *
+ */
 @Slf4j
 @RestController
 public class AdminRestController {
-	
+
 	@Autowired
 	AdminService adminService;
+
+	/**
+	 * @param p_Article {@link Article}
+	 * @return article {@link Article}
+	 */
 	
-	public createArticle(Article ) {
+	public Article createArticle(Article p_Article) {
+
+		Article hArticle = adminService.createArticleAdmin(p_Article);
+
+		log.info("Article créé");
+
+		return hArticle;			
+
+	}
+
+	/**
+	 * @param p_Article {@link Article}
+	 * @return article {@link Article}
+	 */
+	public Article updateArticle(Article p_Article) {
+
+		Article uArticle = adminService.updateArticleAdmin(p_Article);
+
+		return uArticle;
+
+	}
+	
+	/**
+	 * @param p_Article {@link Article}
+	 * @return article {@link Article}
+	 */
+	public void deleteArticle(Integer idArticle) {
 		
-	return Article	
+	adminService.removeArticleAdmin(idArticle);
 		
 	}
-   
-	
 	// read One
-	
+
 	// read All  ( select - get )
-	
-	
-	/////////////////
-	// Create (insert - put)
-	
-	// methode put
-	
-	//////////////////////
-	
-	// a recuperer dans Article 
-	
-	// delete
-	
 }
