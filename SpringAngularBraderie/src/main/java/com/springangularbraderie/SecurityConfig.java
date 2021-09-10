957d7bc2aa61a25995de8c19a36182e595cb18c1
savate974 <stephane.kouotze@gmail.com>
2021-09-10 10:06:22 +0200

    MAJ diverses // Change SecurityConfig

--- SpringAngularBraderie/src/main/java/com/springangularbraderie/SecurityConfig.java
+++ SpringAngularBraderie/src/main/java/com/springangularbraderie/SecurityConfig.java
@@ -4,10 +4,7 @@
 package com.springangularbraderie;
 
 import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -15,86 +12,106 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import com.springangularbraderie.service.AuthentificationService;
-
-import lombok.extern.slf4j.Slf4j;
-import sun.util.logging.resources.logging;
 
 /**
- * @author 31010-69-04
+ * @author JRSS
+ * Fichier de configuration
  *
  */
 
 // on indique fichier de configuration spring
 // @EnableWebSecurity permet de valider les modifs faite par la classe abstraite WebSecurityConfigurerAdapter
 // @EnableGlobalMethodSecurity The prePostEnabled property enables Spring Security pre/post annotations.
-
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-@Slf4j
+@EnableGlobalMethodSecurity(securedEnabled=true)
 // WebSecurityConfigurerAdapter permet d'overrider les sécurité web avec les notres
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private AuthentificationService hAuthentificationService;
+	//        @Bean
+	//        CorsConfigurationSource corsConfigurationSource() {​​​​​​​
+	//            CorsConfiguration config = new CorsConfiguration();
+	//            config.setAllowedOrigins(Arrays.asList("http://localhost:4200","https://localhost:9000"));
+	//            config.setAllowedMethods(Arrays.asList("GET","POST"));
+	//            config.setAllowCredentials(true);
+	//            //the below three lines will add the relevant CORS response headers
+	//            config.addAllowedOrigin("http://localhost:4200");
+	//            config.addAllowedOrigin("http://localhost:9000");
+	//            config.addAllowedHeader("*");
+	//            config.addAllowedMethod("*");
+	//            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	//            source.registerCorsConfiguration("/**", config);
+	//            return source;
+	//        }​​​​​​​
 
-	@Bean
-	public PasswordEncoder passwordEncoder(){
-		return new BCryptPasswordEncoder();		
+	@Autowired
+	public void globalConfig(AuthenticationManagerBuilder auth) throws Exception {
+		//Authentification en mémoire static
+		auth.inMemoryAuthentication().withUser("Admin").password("{​​​​​​​noop}​​​​​​​admin132").roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("Marleyb").password("{​​​​​​​noop}​​​​​​​marleyb123").roles("USER");
+		auth.inMemoryAuthentication().withUser("Charliep").password("{​​​​​​​noop}​​​​​​​charleip123").roles("USER");
+		auth.inMemoryAuthentication().withUser("Milesd").password("{​​​​​​​noop}​​​​​​​milesd123").roles("USER");
+		auth.inMemoryAuthentication().withUser("Keithj").password("{​​​​​​​noop}​​​​​​​keithj123").roles("USER");
+
 	}
-	
-	@Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200","https://localhost:9000"));
-        config.setAllowedMethods(Arrays.asList("GET","POST"));
-        config.setAllowCredentials(true);
-        //the below three lines will add the relevant CORS response headers
-        config.addAllowedOrigin("http://localhost:4200");
-        config.addAllowedOrigin("http://localhost:9000");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return source;
-    }
 
 	// on configure nos servcies HTTP en lui passant les méthodes accessibles 
 	// (GEt/ post) dans les antMatchers ( qui coreespondent au URl de notre appli ainsi que le role de l'accebilité
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.antMatchers("magasin/**").permitAll()
-//		.and()
-//		.formLogin()
-//		.loginProcessingUrl("/index/user")
-//		.usernameParameter("login")
-//        .passwordParameter("pass")
-//		.defaultSuccessUrl("/magasin/**", true).permitAll()
-//		.failureUrl("/robert.html?error=true")
-//		.antMatchers("/SpringAngularBraderie/**").hasRole("admin")
-//		.antMatchers(HttpMethod.GET, "/magasin/getAllArticle/**").hasRole("user")
-//		.antMatchers(HttpMethod.GET, "/magasin/getListPanier").hasRole("user")
-//		.antMatchers(HttpMethod.GET, "/magasin/getArticle/**").hasRole("user")
-//		.antMatchers(HttpMethod.POST, "/magasin/savePanier/**").hasRole("user")
-//		.antMatchers(HttpMethod.POST, "/index/user/**").hasRole("user")
-//		.antMatchers(HttpMethod.DELETE, "/magasin/clear/**").hasRole("user")
-//		.antMatchers(HttpMethod.DELETE, "caddie/removeArticle/**").hasRole("user")	
-		.and().httpBasic();
+		//Instanciation d'un cors config
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200/"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setExposedHeaders(Arrays.asList("Authorization"));
+		/**
+		 * Sécurisation des APIRest:
+		 * pour les rôles admin pas de restrictions
+		 * pour les rôles USER restriction sur les PUT, PATCH et DELETE
+		 */
+		//http.cors();
+		http
+		//HTTP Basic authentication
+		.httpBasic()
+		.and()
+		.authorizeRequests()
+		.antMatchers("/**").hasRole("ADMIN")
+		.antMatchers(HttpMethod.GET, "/magasin/getAllArticle/**").hasRole("USER")
+		.antMatchers(HttpMethod.GET, "/magasin/getListPanier").hasRole("USER")
+		.antMatchers(HttpMethod.GET, "/magasin/getArticle/**").hasRole("USER")
+		.antMatchers(HttpMethod.POST, "/magasin/savePanier/**").hasRole("ADMIN")
+		.antMatchers(HttpMethod.POST, "/index/user/**").hasRole("USER")
+		.antMatchers(HttpMethod.DELETE, "/magasin/clear/**").hasRole("USER")
+		.antMatchers(HttpMethod.DELETE, "caddie/removeArticle/**").hasRole("USER")    
+		.and()
+		.csrf().disable()
+		.formLogin().disable();
 
-		http.csrf().disable().cors().configurationSource(corsConfigurationSource());
-		http.headers().frameOptions().disable();
-	}
-
-	@Override
-	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-		auth.eraseCredentials(true).userDetailsService(hAuthentificationService).passwordEncoder(passwordEncoder());
+		// You can customize the following part based on your project, it's only a sample
+		//        http
+		//        .authorizeRequests()
+		//        .antMatchers("/**")
+		//        .permitAll()
+		//        .anyRequest()
+		//        .authenticated()
+		//        .and()
+		//        .csrf()
+		//        .disable()
+		//        .cors()
+		//        .configurationSource(request -> corsConfiguration);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
--- SpringAngularBraderie/src/main/java/com/springangularbraderie/restcontroller/UserRestController.java
+++ SpringAngularBraderie/src/main/java/com/springangularbraderie/restcontroller/UserRestController.java
@@ -3,9 +3,6 @@
  */
 package com.springangularbraderie.restcontroller;
 
-
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,7 +27,6 @@ import lombok.extern.slf4j.Slf4j;
 @CrossOrigin(origins ="*")
 public class UserRestController {
 
-
 	@Autowired
 	IUserService hUserService;
 	
@@ -43,17 +39,12 @@ public class UserRestController {
 	 * @return User {@link User}
 	 */
 	@PostMapping(path="/user", produces= "application/json")
-	public Account getCurrentConnectUser(@RequestBody Map<String, String> json) {
+	public Account getCurrentConnectUser(@RequestBody Account user) {
 		
-		String login = json.get("login");
-		
-		String pass = json.get("pass");
-		
-		Account p_user = hUserService.enableTolog(login, pass);
+		Account p_user = hUserService.enableTolog(user.getLogin(), user.getPass());
 	
 		log.info("user authentifié : " + p_user);
 		
 		return p_user;
 	}
-	
 }
